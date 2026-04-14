ee8f107831672ddb0d5fbc73c9c784e44f509748
Ramadhany Kurnia Pratama <ramadhanykurniapratama@gmail.com>
2026-04-14 14:40:43 +0700

    Fix testing unit script

--- src/test_script.py
+++ src/test_script.py
@@ -1,7 +1,7 @@
 import os
 
 def test_data_exists():
-    assert os.path.exists("data/iris.csv"), "File data tidak ditemukan!"
+    assert os.path.exists("data/Iris.csv"), "File data tidak ditemukan!"
 
 def test_mlflow_dir():
     # Memastikan folder mlruns tercipta setelah training
