88804f60a83354ba77f5cd0fb54da9dd137635ee
Ramadhany Kurnia Pratama <ramadhanykurniapratama@gmail.com>
2026-04-14 15:07:15 +0700

    Fix target training script

--- src/train.py
+++ src/train.py
@@ -13,8 +13,8 @@ def run_train():
     with mlflow.start_run():
         # Load Data
         df = pd.read_csv('data/Iris.csv')
-        X = df.drop('species', axis=1)
-        y = df['species']
+        X = df.drop('Species', axis=1)
+        y = df['Species']
         
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
