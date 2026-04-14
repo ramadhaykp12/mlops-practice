fd6ce15f47ded010f5ac1e2f81574eb7cc7bdac4
Ramadhany Kurnia Pratama <ramadhanykurniapratama@gmail.com>
2026-04-14 15:25:52 +0700

    add several update file

---
+++ src/test_script.py
@@ -0,0 +1,9 @@
+import os
+
+def test_data_exists():
+    assert os.path.exists("data/Iris.csv"), "File data tidak ditemukan!"
+
+def test_mlflow_dir():
+    # Memastikan folder mlruns tercipta setelah training
+    if os.path.exists("src/train.py"):
+        assert True
